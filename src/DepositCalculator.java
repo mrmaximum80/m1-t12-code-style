1033febc6acfcc9e8b1b803b46784d511ca4373d
mrmaximum80 <mrmaximum@yandex.ru>
2023-01-10 23:21:50 +0300

    Для пир-ревью

---
+++ src/DepositCalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return roundValue(pay, 2);
+    }
+
+    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
+        return roundValue(amount + amount * yearRate * depositPeriod, 2);
+    }
+
+    double roundValue(double value, int decimalPlaces) {
+        double scale = Math.pow(10, decimalPlaces);
+        return Math.round(value * scale) / scale;
+    }
+
+    void openDeposit() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+        System.out.println("Введите срок вклада в годах:");
+        int depositPeriod = scanner.nextInt();
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        int action = scanner.nextInt();
+        double result = 0;
+        if (action == 1) {
+            result = calculateSimplePercent(amount, 0.06, depositPeriod);
+        } else if (action == 2) {
+            result = calculateComplexPercent(amount, 0.06, depositPeriod);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + depositPeriod + " лет превратятся в " + result);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().openDeposit();
+    }
+}
